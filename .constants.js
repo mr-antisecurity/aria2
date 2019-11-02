8959f8c671954ad2781dcd3e62674810c0b58235
therealme1 <57095110+therealme1@users.noreply.github.com>
2019-11-03 00:56:44 +0530

    Update .constants.js

--- .constants.js
+++ .constants.js
@@ -7,7 +7,7 @@ module.exports = Object.freeze({
   ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
   GDRIVE_PARENT_DIR_ID: '1GJGcH16p94FDe8E7gzkTc6zrI0H8SRg2',
   SUDO_USERS: [464848593, 401643149],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [23543, 3535],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  AUTHORIZED_CHATS: [-329690821],	// Telegram chat IDs. Anyone in these chats can use the bot.
   DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
     enabled: false,   // Set this to true to use the notify functionality
     host: 'hostname.domain',
